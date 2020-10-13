0b32850ab7161cf1e7721c0395d4a4b87db8f42d
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-13 20:06:31 +0200

    Add complimentary solution

--- src/arrayExtension.js
+++ src/arrayExtension.js
@@ -20,20 +20,28 @@
  * @property {number} points - The points.
  */
 
- // ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 //  Public interface
 // ------------------------------------------------------------------------------
 
 /**
  * Returns the name of the students whose points is greater than,
  * or equal to, the average of the points.
- * 
+ *
  * @param {Student[]} students - Students to filter.
  * @throws {TypeError} - Throws 'The passed argument is not an array.' if the argument is not an array.
  * @returns {string[]} The name of the students better than average.
  */
 export function filterBetterThanAverage (students) {
-  if (!Array.isArray()) {
+  if (!Array.isArray(students)) {
     throw new TypeError('The passed argument is not an array.')
   }
+
+  const averagePoints = students.reduce((a, student) => a + student.points, 0) / students.length
+
+  const theBest = students
+    .filter(student => student.points >= averagePoints)
+    .map(student => student.name)
+
+  return theBest
 }
